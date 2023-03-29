a6a9bef22922b0c94e60e7105fe08ab3cbfdb4b1
AllanDevincolagit <79774220+AllanDevincolagit@users.noreply.github.com>
2023-03-29 18:16:33 -0300

    Carrosel React ProdVariados
    Carrosel em ReactJs na página de produtos diversos iniciado

---
+++ Web/prod_personalizados/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="carousel">
+      <div className="item">
+
+        <div className="image">
+          <img src="" alt="Camisa"></img>
+        </div>
+
+        <div className="info">
+          <span className="camisa1">Camisa Branca</span>
+          <span className="OldPrice">R$ 50,00</span>
+          <span className="price">R$ 25,00</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
