a59e62655d40c6fce3040b06e70490a7103e2534
AllanDevincolagit <79774220+AllanDevincolagit@users.noreply.github.com>
2023-03-30 19:48:41 -0300

    Continuação carrosel react
    O carrosel em react foi implementado, porém não integrado ao layout da cammaleão

--- Web/prod_personalizados/src/App.js
+++ Web/prod_personalizados/src/App.js
@@ -1,23 +1,60 @@
-
+import {useEffect, useState, useRef} from 'react'; 
 import './App.css';
 
 function App() {
+
+  const [data,setData] = useState([]);
+  const carousel = useRef(null);
+
+  useEffect(()=>{
+      fetch('http://localhost:3000/static/shoes.json')
+      .then((response) => response.json())
+      .then(setData);
+    }, []);
+
+    const handleLeftClick = (e) =>{
+        e.preventDefault();
+        carousel.current.scrollLeft -= carousel.current.offsetWidth;
+    }
+
+    const handleRightClick = (e) =>{
+        e.preventDefault();
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+    }
+
+    if (!data || !data.length) return null;
+
   return (
-    <div className="carousel">
-      <div className="item">
+    <div className="container">
+      <div className="carousel" ref={carousel}>
 
-        <div className="image">
-          <img src="" alt="Camisa"></img>
-        </div>
+        {data.map((item) => {
+          const {id, name, price, oldPrice, image} = item;
+          return(
+                  <div className="item" key={id}>
 
-        <div className="info">
-          <span className="camisa1">Camisa Branca</span>
-          <span className="OldPrice">R$ 50,00</span>
-          <span className="price">R$ 25,00</span>
-        </div>
+                    <div className="image">
+                      <img src={image} alt={name}></img>
+                    </div>
+
+                    <div className="info">
+                      <span className="name">{name}</span>
+                      <span className="OldPrice">{oldPrice}</span>
+                      <span className="price">{price}</span>
+                    </div>
+                  </div>
+          );
+        })}
+
+      </div>
+
+      <div className='buttons'>
+        <button onClick={handleLeftClick}><img src="/static/images/216151_right_chevron_icon.png" alt="Scroll Left"></img></button>
+        <button onClick={handleRightClick}><img src="/static/images/216151_right_chevron_icon.png" alt="Scroll Right"></img></button>
       </div>
     </div>
   );
 }
+
 
 export default App;
