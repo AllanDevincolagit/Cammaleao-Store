0f3c08ce204bd761ee8ccb9e95915648dff13006
MaiiconnDouglasd <maiicondouglasd@hotmail.com>
2023-04-03 13:57:48 -0300

    Ambiente funcionando!

---
+++ my-web/src/pages/Index/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './index.css';
+
+function Nav() {
+  return (
+    <nav>
+      <div className="logo">
+        <Link to="/">
+          <img src="/public/Favicon.png" alt="Logo" />
+        </Link>
+      </div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Produtos</Link>
+        </li>
+        <li>
+          <Link to="/">Conceitos</Link>
+        </li>
+        <li>
+          <Link to="/">Pedidos Personalizados</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
