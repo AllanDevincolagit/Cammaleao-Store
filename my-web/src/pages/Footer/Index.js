07fbc72d41d6b71cb5c1d1f2bcd460247f589969
MaiiconnDouglasd <maiicondouglasd@hotmail.com>
2023-04-03 14:02:34 -0300

    Footer funcional

---
+++ my-web/src/pages/Footer/Index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: '#fff', padding: '1rem' }}>
+      <p>@ Camaleão Store</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- my-web/src/routes.js
+++ my-web/src/routes.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Index from './pages/Index';
+import Footer from './pages/Footer/Index';
 
 export default function Routes() {
   return(
     <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={Index} /> 
+      <Route path="/" exact component={Index} />     
 
-    </Switch>
+    </Switch> <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <Footer /> 
     </BrowserRouter>
   )
 }
