f8af75396f5ee074f9468b3cc87371095f5852ef
MaiiconnDouglasd <maiicondouglasd@hotmail.com>
2023-04-25 14:02:05 -0300

    Criando user db

--- ECOMMERCE/index.js
+++ ECOMMERCE/index.js
@@ -3,12 +3,16 @@ const dbConnect = require('./config/DbConnect');
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;  // PORTA DO SERVIDOR VAMOS USAR A 4000
+const authRouter = require('./routes/authRoute')
 
 dbConnect();
 
 app.use('/', (req, res) => {
     res.send('Hello From Server Side');
 });
+
+app.use('/api/user', authRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
