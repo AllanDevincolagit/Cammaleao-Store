1c8411f2b14da9589eb4fe09aaaabb4322f472d0
MaiiconnDouglasd <maiicondouglasd@hotmail.com>
2023-04-26 14:02:35 -0300

    Auth

---
+++ ECOMMERCE/controller/userCtrl.js
@@ -0,0 +1,22 @@
+const User = require('../models/userModel');
+
+
+
+const createUser = async (req, res) => {
+  const email = req.body.email;
+  const findUser = await User.findOne(email);
+
+  if (!findUser) {
+
+    // Criando um usuario
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+  } else {
+       res.json({
+         msg: "User Already Exists",
+         success: false,
+       });
+  }
+};
+
+module.exports = { createUser };
--- ECOMMERCE/index.js
+++ ECOMMERCE/index.js
@@ -3,16 +3,20 @@ const dbConnect = require('./config/DbConnect');
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;  // PORTA DO SERVIDOR VAMOS USAR A 4000
-const authRouter = require('./routes/authRoute')
+const authRouter = require('./routes/authRoute');
+const bodyParser = require('body-parser');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 
 dbConnect();
 
-app.use('/', (req, res) => {
-    res.send('Hello From Server Side');
-});
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api/user', authRouter);
 
+app.use(notFound);
+app.request(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
