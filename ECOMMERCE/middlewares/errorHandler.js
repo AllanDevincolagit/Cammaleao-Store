db0d3be7fddbf94f04e15f985dc6b42cd24d8179
MaiiconnDouglasd <maiicondouglasd@hotmail.com>
2023-05-08 01:58:10 -0300

    authmiddçeware create

--- ECOMMERCE/middlewares/errorHandler.js
+++ ECOMMERCE/middlewares/errorHandler.js
@@ -1,20 +1,21 @@
-// Not Found
+// not Found
 
-const notFound = ( req, res, next ) => {
-  const error = new Error(`Not Found: ${req.originaUrl}`);
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found : ${req.originalUrl}`);
   res.status(404);
   next(error);
-}
+};
 
 // Error Handler
 
-const errorHandler = ( err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   const statuscode = res.statusCode == 200 ? 500 : res.statusCode;
   res.status(statuscode);
   res.json({
+    status: "fail",
     message: err?.message,
     stack: err?.stack,
   });
-}
+};
 
 module.exports = { errorHandler, notFound };
