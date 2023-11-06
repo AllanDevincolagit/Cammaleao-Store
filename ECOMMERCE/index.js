1870a2a9d9b66a6b2ab6382ef5df7a286d3c375a
MaiiconnDouglasd <maiicondouglasd@hotmail.com>
2023-11-06 01:33:47 -0300

    updates #

--- ECOMMERCE/index.js
+++ ECOMMERCE/index.js
@@ -1,20 +1,17 @@
-const express = require('express');
-const dbConnect = require('./config/dbConnect');
+const express = require("express");
+const dbConnect = require("./config/dbConnect");
 const app = express();
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;  // PORTA DO SERVIDOR VAMOS USAR A 4000
-const authRouter = require('./routes/authRoute');
-const bodyParser = require('body-parser');
-const { notFound, errorHandler } = require('./middlewares/errorHandler');
+const authRouter = require("./routes/authRoute");
+const bodyParser = require("body-parser");
+
 dbConnect();  // Conecta com o banco de dados
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("api/user", authRouter);
-
-app.use(notFound); 
-app.use(errorHandler);
+app.use("/api/user", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
